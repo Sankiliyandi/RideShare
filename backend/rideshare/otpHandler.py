d606c605ef179dbf4a9077a2310926e187ebd71b
sathishviper <d.r.e.a.m.e.r.sathish@gmail.com>
2023-03-17 20:51:06 +0530

    mobile res

--- backend/rideshare/otpHandler.py
+++ backend/rideshare/otpHandler.py
@@ -4,11 +4,11 @@ from twilio.rest import Client
 def otp(phoneNo,code):
     
     acc_sid="ACf9ed85edb96f70568990c0fdca030946"
-    token="a9981f5c240cd1be869913999c4d7380"
+    token="0bdd0423b2beaf346bf912a7f7e70d04"
     client=Client(acc_sid,token)
     messageOTP=client.messages.create(
         body='your RideShare registration otp:'+code,
         from_="+12344075073",
         to=phoneNo
     )
-    print(messageOTP.sid)
+    print(messageOTP.sid) 
